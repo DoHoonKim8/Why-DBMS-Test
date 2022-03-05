5d568023b6f13196811e7bb010e77bec67457265
DoHoonKim <dohoon@pi-lab.io>
2022-03-06 05:19:01 +0900

    Fucking shit

--- project1.cc
+++ project1.cc
@@ -1,6 +1,38 @@
 // Copyright 2022 Wook-shin Han
+#include <istream>
+#include <string>
+#include "project1.h"
 
-int main(int argc, char** argv) {
-	/* fill this */
-	return 0;
+int main(int argc, char* argv[]) {
+  /* if (std::string(argv[1]) == "q1") {
+    std::ifstream customer_file(argv[2]);
+    CustomerTable customer_table;
+    customer_table.load_file(customer_file);
+    // customer_table.show();
+    customer_file.close();
+
+    std::ifstream zonecost_file(argv[3]);
+    ZonecostTable zonecost_table;
+    zonecost_table.load_file(zonecost_file);
+    // zonecost_table.show();
+    zonecost_file.close();
+
+    Query::Query1(customer_table, zonecost_table);
+
+  } else if (std::string(argv[1]) == "q2") {
+    std::ifstream lineitem_file(argv[2]);
+    LineitemTable lineitem_table;
+    lineitem_table.load_file(lineitem_file);
+    // lineitem_table.show();
+    lineitem_file.close();
+
+    std::ifstream products_file(argv[3]);
+    ProductsTable products_table;
+    products_table.load_file(products_file);
+    // products_table.show();
+    products_file.close();
+
+    Query::Query2(lineitem_table, products_table);
+  }*/
+  return 0; 
 }
--- project1.h
+++ project1.h
@@ -2,5 +2,88 @@
 
 #ifndef WHY_DBMS_PROJECT1_H_
 #define WHY_DBMS_PROJECT1_H_
+#include <string>
+#include <vector>
+#include <variant>
+#include <utility>
+#include <optional>
+#include <tuple>
+#include <unordered_map>
+
+enum Status {
+    PASSIVE,
+    ACTIVE
+};
+
+template<typename T>
+class Column {
+private:
+    std::string name_;
+    int max_length_;
+    std::vector<std::optional<T>> datas_; // QUESTION : Is this a nice choice?
+
+public:
+    using Type = T; // denotes Column's type
+
+    Column(std::string name, int max_length)
+        : name_(name), max_length_(max_length) {}
+
+    void InsertData(T data) {
+        datas_.push_back(data);
+    }
+};
+
+template<typename... Types>
+class Table {
+protected:
+    std::string table_name_;
+    std::tuple<Column<Types>...> columns_;
+
+    Table(std::string table_name, std::tuple<Column<Types>...> columns)
+        : table_name_(table_name), columns_(columns) {}
+
+    using DataType = std::variant<Types...>;
+
+    void InsertRecord(std::vector<DataType> record) {
+        for (int i = 0; i < record.size(); i++) {
+            columns_[i].InsertData(record[i]);
+        }
+    }
+};
+
+class CustomerTable 
+    : public Table<
+        std::string,
+        std::string,
+        std::string,
+        std::string,
+        std::string,
+        short,
+        char,
+        short,
+        short,
+        double,
+        int,
+        std::string,
+        Status> {
+public:
+    CustomerTable()
+        : Table("Customer",
+                std::make_tuple(
+                    Column<std::string>("UNAME", 20),
+                    Column<std::string>("PASSWD", 20),
+                    Column<std::string>("LNAME", 20),
+                    Column<std::string>("FNAME", 20),
+                    Column<std::string>("ADDR", 50),
+                    Column<short>("ZONE", 6),
+                    Column<char>("SEX", 3),
+                    Column<short>("AGE", 6),
+                    Column<short>("LIMIT", 6),
+                    Column<double>("BALANCE", 10),
+                    Column<int>("CREDITCARD", 20),
+                    Column<std::string>("EMAIL", 50),
+                    Column<Status>("ACTIVE", 6)
+                )) {}
+};
 
 #endif  // WHY_DBMS_PROJECT1_H_
